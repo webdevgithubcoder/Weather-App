6515517d012731dbfa5b3ecbd8ed34a075ce6cf7
webdevgithubcoder <100611006+webdevgithubcoder@users.noreply.github.com>
2025-04-02 22:29:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -33,19 +33,19 @@ console.log(weather_data);
 
  switch(weather_data.weather[0].main){
    case 'Clouds':
-      weather_img.src = "/images/cloud.png";
+      weather_img.src = "images/cloud.png";
       break;
    case 'Clear':
-      weather_img.src = "/images/clear.png";
+      weather_img.src = "images/clear.png";
       break;
    case 'Rain':
-      weather_img.src = "/images/rain.png";
+      weather_img.src = "images/rain.png";
       break;
    case 'Rain':
-      weather_img.src = "/images/mist.png";
+      weather_img.src = "images/mist.png";
       break;
    case 'Snow':
-      weather_img.src = "/images/snow.png";
+      weather_img.src = "images/snow.png";
       break;
  
  }
